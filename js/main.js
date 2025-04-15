e475340a8de669dfe164bb604433844dc9fccc8b
meliiikke <melikecengizz1598@gmail.com>
2025-04-15 16:24:20 +0300

    modal-dialog

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import headerFunc from "./header.js";
 import productsFunc from "./products.js";
-import searchFunc from "./search.js"
+import searchFunc from "./search.js";
 
 //! add product to localStorage start
 
@@ -10,7 +10,7 @@ async function getData() {
 
   data ? localStorage.setItem("products", JSON.stringify(data)) : [];
   productsFunc();
-  searchFunc(data)
+  searchFunc(data);
 }
 
 getData();
@@ -24,4 +24,26 @@ cartItems.innerHTML = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart")).length
   : "0";
 
-  //! add cartItem to localStorage end
+//! add cartItem to localStorage end
+
+//! modal dialog start
+const modalDialog = document.querySelector(".modal-dialog");
+const btnCloseDialog = document.querySelector(".modal-dialog .modal-close");
+const modalContentDOM = document.querySelector(".modal-dialog .modal-content");
+
+
+
+btnCloseDialog.addEventListener("click", function () {
+  modalDialog.classList.remove("show");
+});
+
+document.addEventListener("click", (e) => {
+  if(!e.composedPath().includes(modalContentDOM)){
+    modalDialog.classList.remove("show");
+  }
+});
+setTimeout(() => {
+  modalDialog.classList.add("show");
+}, 2000);
+
+//! modal dialog end
